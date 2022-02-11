8c9c727967e58764e1517f24c7095373c265ee06
Giao The Cung <giaothe@gmail.com>
2022-02-11 14:40:09 +0100

    Remove bad copy and paste
    https://jira.adeo.no/browse/DAG-133

--- src/main/kotlin/no/nav/dagpenger/mellomlagring/App.kt
+++ src/main/kotlin/no/nav/dagpenger/mellomlagring/App.kt
@@ -1,7 +1,6 @@
 package no.nav.dagpenger.mellomlagring
 
 import io.ktor.application.install
-import io.ktor.auth.Authentication
 import io.ktor.features.CallLogging
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -10,17 +9,12 @@ import no.nav.dagpenger.mellomlagring.api.vedleggApi
 import no.nav.dagpenger.mellomlagring.crypto.AESCrypto
 import no.nav.dagpenger.mellomlagring.lagring.S3Store
 import no.nav.dagpenger.mellomlagring.lagring.VedleggService
-import no.nav.security.token.support.ktor.tokenValidationSupport
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
         install(CallLogging) {
             disableDefaultColors()
         }
-        install(Authentication) {
-            tokenValidationSupport(config = environment.config)
-        }
-
         health()
         vedleggApi(
             VedleggService(
