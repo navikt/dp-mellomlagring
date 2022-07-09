6321ac627998828a3b8125c61e9d40bdd6d61240
Giao The Cung <giaothe@gmail.com>
2022-07-09 20:58:32 +0200

    Skille mellom ktor plugins og API
    Co-authored-by: rannveigskjerve <rannveig.alette.skjerve@nav.no>

--- src/test/kotlin/no/nav/dagpenger/mellomlagring/TestApplication.kt
+++ src/test/kotlin/no/nav/dagpenger/mellomlagring/TestApplication.kt
@@ -3,13 +3,8 @@ package no.nav.dagpenger.mellomlagring
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.header
 import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
 import io.ktor.server.application.Application
 import io.ktor.server.testing.ApplicationTestBuilder
-import io.ktor.server.testing.TestApplicationCall
-import io.ktor.server.testing.TestApplicationEngine
-import io.ktor.server.testing.TestApplicationRequest
-import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.testApplication
 import no.nav.security.mock.oauth2.MockOAuth2Server
 
@@ -47,7 +42,10 @@ internal object TestApplication {
             System.setProperty("TOKEN_X_WELL_KNOWN_URL", "${mockOAuth2Server.wellKnownUrl(Config.TokenX.name)}")
             System.setProperty("AZURE_APP_WELL_KNOWN_URL", "${mockOAuth2Server.wellKnownUrl(Config.AzureAd.name)}")
             testApplication {
-                application(moduleFunction)
+                application {
+                    ktorFeatures()
+                    moduleFunction()
+                }
                 test()
             }
         } finally {
@@ -59,15 +57,5 @@ internal object TestApplication {
         xEier?.let {
             this.header("X-Eier", xEier)
         }
-    }
-
-    internal fun TestApplicationEngine.autentisert(
-        endepunkt: String,
-        token: String = tokenXToken,
-        httpMethod: HttpMethod = HttpMethod.Get,
-        setup: TestApplicationRequest.() -> Unit = {}
-    ): TestApplicationCall = handleRequest(httpMethod, endepunkt) {
-        addHeader(HttpHeaders.Authorization, "Bearer $token")
-        setup()
     }
 }
