a253f90cc7ea62a46ff72e8c933f149314875e75
Giao The Cung <giaothe@gmail.com>
2026-09-24 14:23:48 +0200

    fix: Fikset feilende bygg av docker image
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "2.4.20"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+    alias(libs.plugins.shadow.jar)
     application
 }
 
